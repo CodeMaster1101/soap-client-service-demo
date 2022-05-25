c0975247206199a8b51a3982d7287ce462760dd9
Mile Stanislavov <cpthermes@gmail.com>
2022-05-25 10:22:03 +0200

    modified the client

--- collection-management-soap/src/main/java/com/mile/collection/management/soap/bottomUp/Question.java
+++ collection-management-soap/src/main/java/com/mile/collection/management/soap/bottomUp/Question.java
@@ -3,35 +3,19 @@ package com.mile.collection.management.soap.bottomUp;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlType;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.stereotype.Component;
 
 
 @Table(value = "Question")
-@XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "question", propOrder = {
-    "answers",
-    "categories",
-    "id",
-    "value"
-})
 @Component
 public class Question {
 	
-    @Id 
-    @XmlElement(required = true)
+    @Id
 	private Long id;
-    @XmlElement(required = true)
 	private String value;
-    @XmlElement(nillable = true)
 	private Set<Answer> answers = new HashSet<>();
-    @XmlElement(nillable = true)
 	private Set<Category> categories = new HashSet<>();
 
 	public Question() {}
